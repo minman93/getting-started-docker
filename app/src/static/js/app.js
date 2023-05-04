822fad84f74d2d108d1a1d02eea71d11796f4660
Matt Inman <mattinman@Ians-iMac.local>
2023-05-04 12:00:48 +0100

    push 2

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add an item!'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
